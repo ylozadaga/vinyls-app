f0a056bb1ca4bc2ea8e8533bc5ec97ca4a147db0
personal <y.lozada@uniandes.edu.co>
2022-11-05 10:04:00 -0500

    initial scafolding

---
+++ app/src/main/java/com/team31/vinyls/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.team31.vinyls
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
