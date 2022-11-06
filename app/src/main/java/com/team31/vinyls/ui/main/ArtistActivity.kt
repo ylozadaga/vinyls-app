767a8b30c2462e8531e959a89e9bad197bd71afc
Fredy Alejandro Russi Fajardo <frussi@asesoftware.com>
2022-11-06 14:12:07 -0500

    feature/HU03: se agrega home y menu de Visitante sin conexion

---
+++ app/src/main/java/com/team31/vinyls/ui/main/ArtistActivity.kt
@@ -0,0 +1,12 @@
+package com.team31.vinyls.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team31.vinyls.R
+
+class ArtistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist)
+    }
+}
